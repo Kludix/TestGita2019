1549323adcb60fb5e1ef0705b2e2a7422bc3b0ee
mlZPO <mlatka@poczta.onet.pl>
2019-10-13 22:42:47 +0200

    second change addition

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("BLA BLA BLA");
         System.out.println("Ala ma kota");
+        System.out.println("Test2");
     }
 }
